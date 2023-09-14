84ec3e7f05144b5c4985bc94165a1458a72cd3bb
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2023-09-14 14:24:46 +0100

    cypress test for checking number of list items after sign in is correct

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -19,3 +19,18 @@ describe("Arrives at login page and can log in", () => {
     cy.contains("Sign Out").click();
   });
 });
+
+describe("After login, page should display 37 species of characters", () => {
+  it("logs in and checks for number of species", function () {
+    //goes to site
+    cy.visit("http://localhost:5173/");
+    //finds username input box and types email
+    cy.get("input[name=username]").type("test@test.com");
+    //finds password input box and inputs password and presses enter(submit) after
+    cy.get("input[name=password]").type("test123{enter}");
+    //checks to see if there are 37 occurences of the li tag
+    cy.get("li").should("have.length", 37);
+    //signs out after running the above
+    cy.contains("Sign Out").click();
+  });
+});
