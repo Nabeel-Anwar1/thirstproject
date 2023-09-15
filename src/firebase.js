64df1bf1f1fbba63d4d429f22b61c9a5cc291f4d
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2023-09-15 16:58:56 +0100

    used dotenv for api keys

--- src/firebase.js
+++ src/firebase.js
@@ -10,12 +10,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAvPiA8peFyA7OaCyvdKOavERzFC7RBxHo",
-  authDomain: "thirstproj.firebaseapp.com",
-  projectId: "thirstproj",
-  storageBucket: "thirstproj.appspot.com",
-  messagingSenderId: "460856015831",
-  appId: "1:460856015831:web:dcf47a7dd9e67e12bc6992",
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
 };
 
 // Initialize Firebase
